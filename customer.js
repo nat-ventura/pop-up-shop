250a8aeb712270f28bc97cf928ab8cdbd78af332
Natalie Villasana <navillasa@gmail.com>
2017-08-10 11:40:06 -0400

    adds ability to save customer to database--- but also realized i'm using node version 4.8 :((( need to change with nvm

--- customer.js
+++ customer.js
@@ -5,14 +5,27 @@ const dbConfig = {
     username: process.env.DB_USER,
     database: process.env.DB_NAME
 };
-const db = pg(dbConfig);
 
 class Customer {
     constructor(name, email, addr, password) {
-
+        this.db = pg(dbConfig);
+        this.name = name;
+        this.email = email;
+        this.address = addr;
+        this.password = password;
     }
     save() {
-
+        // the safe way is to go into postgres,
+        // type out a query, see if it works,
+        // then copy and paste
+        // OR.....
+        return this.db.query(`
+        insert into customers
+        (name, email, address, password)
+        values
+        ('${this.name}','${this.email}','${this.address}','${this.password}');
+        `);
+        // line 26 allows you to inject javascript -- it's string interpolation
     }
 }
 
--- test/test-customer-model.js
+++ test/test-customer-model.js
@@ -2,14 +2,28 @@ const expect = require('chai').expect;
 const Customer = require('../customer');
 
 describe('Customers', () => {
-    it('should be able to save to the database', () => {
+    it('should be able to save to the database',
+    (done) => {
 
         let myCustomer = new Customer('me', 'me@me.com', '123 me street', 'm3m3m3');
         // if you pass these into the constructor,
         // the constructor should be ready to RECEIVE THEM
+
+        // in any of your models,
+        // you should be able to
+        // Create
+        // Retrieve -- using SQL, this would be select -- a surefire way to identify is
+                                    // whatever you used as its primary key
+                                    // so now we need to find out,
+                                    // when we write it to the database, do we find out?
+        // Update
+        // Delete
+
         myCustomer
             .save()
-            .then(done);
-        expect(1 + 1).to.equal(2);
+            .then((result) => {
+                
+                done();
+            });
+        });
     });
-});
