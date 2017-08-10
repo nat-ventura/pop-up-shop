5aaf761d695b44156207235b4c39f22788c97f20
Natalie Villasana <navillasa@gmail.com>
2017-08-10 11:11:36 -0400

    added host, username, and database to dbConfig in customer.js

--- customer.js
+++ customer.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME
+};
+const db = pg();
 
 class Customer {
     constructor() {
